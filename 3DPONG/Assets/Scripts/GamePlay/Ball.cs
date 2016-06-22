fa38cbbcd15549173694dec81ba942334dad99ab
Jorge Moreno <jmoreno@cgoswell.edu>
2016-06-21 21:40:45 -0700

    Boucningise

--- 3DPONG/Assets/Scripts/GamePlay/Ball.cs
+++ 3DPONG/Assets/Scripts/GamePlay/Ball.cs
@@ -9,28 +9,32 @@ public class Ball : MonoBehaviour {
     private Rigidbody BallRb;
 
     private Vector3 moveTo;
-
+    private Vector3 moveFrom;
     public void Start()
     {
         moveTo = Vector3.right;
+        moveFrom = Vector3.forward;
         BallRb = GetComponent<Rigidbody>();
         BallRb.maxAngularVelocity = MaxAngularVelocity;
+        BallRb.useGravity = false;
+        BallRb.AddForce(Vector3.one * speed);
     }
 	// Update is called once per frame
-	public void LateUpdate () 
+	public void FixedUpdate () 
     {
         
         //Vector3 pos = new Vector3(0, 0, 0);
 	    if(BallRb.maxAngularVelocity != MaxAngularVelocity)
             BallRb.maxAngularVelocity = MaxAngularVelocity;
-        Movement(moveTo);
+       // Movement(moveTo, moveFrom);
     }
 
-    private void Movement(Vector3 m)
+    private void Movement(Vector3 m, Vector3 u)
     {
         if (useTorque)
         {
-            BallRb.AddTorque(m * speed);
+            BallRb.AddRelativeTorque(m * speed * Time.deltaTime);
+            BallRb.AddRelativeForce((u * Random.Range(-1.0f, 1.0f)) * speed * Time.deltaTime);
         }
         else
             BallRb.AddForce(new Vector3(m.z, m.y, m.x));
@@ -41,14 +45,16 @@ public class Ball : MonoBehaviour {
         if(other.collider.tag == "Player")
         {
             moveTo = Vector3.right;
-            MaxAngularVelocity += 1.5f;
-            speed += 1.0f;
+            moveFrom = Vector3.forward;
+            //MaxAngularVelocity += 1.5f;
+           // speed += 1.0f;
         }
         else if(other.collider.tag == "Enemy")
         {
             moveTo = Vector3.left;
-            MaxAngularVelocity += 1.5f;
-            speed += 1.0f;
+            moveFrom = Vector3.back;
+           // MaxAngularVelocity += 1.5f;
+           // speed += 1.0f;
 
         }
     }

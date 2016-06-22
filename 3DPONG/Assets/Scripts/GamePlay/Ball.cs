6c00c55cde8097d56d2d702563c7f2fa63195ceb
Jorge Moreno <jmoreno@cgoswell.edu>
2016-06-21 22:13:47 -0700

    Changes

--- 3DPONG/Assets/Scripts/GamePlay/Ball.cs
+++ 3DPONG/Assets/Scripts/GamePlay/Ball.cs
@@ -3,59 +3,38 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-    public float speed;
-    public bool useTorque;
-    public float MaxAngularVelocity = 25;
+    public Vector3 speed;
     private Rigidbody BallRb;
-
-    private Vector3 moveTo;
-    private Vector3 moveFrom;
+    public float maxAngularSpeed;
     public void Start()
     {
-        moveTo = Vector3.right;
-        moveFrom = Vector3.forward;
         BallRb = GetComponent<Rigidbody>();
-        BallRb.maxAngularVelocity = MaxAngularVelocity;
         BallRb.useGravity = false;
-        BallRb.AddForce(Vector3.one * speed);
+        BallRb.AddRelativeForce(Vector3.one * 500);
+        BallRb.maxAngularVelocity = maxAngularSpeed;
     }
-	// Update is called once per frame
-	public void FixedUpdate () 
+    // Update is called once per frame
+    public void FixedUpdate()
     {
-        
-        //Vector3 pos = new Vector3(0, 0, 0);
-	    if(BallRb.maxAngularVelocity != MaxAngularVelocity)
-            BallRb.maxAngularVelocity = MaxAngularVelocity;
-       // Movement(moveTo, moveFrom);
+        Ray ray = new Ray(this.gameObject.transform.position, BallRb.velocity * 15.0f);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.gameObject.name =="Player" || hit.collider.gameObject.name == "Enemy")
+            {
+                transform.Rotate(Vector3.one);
+                speed = BallRb.velocity;
+            }
+        }
     }
 
     private void Movement(Vector3 m, Vector3 u)
     {
-        if (useTorque)
-        {
-            BallRb.AddRelativeTorque(m * speed * Time.deltaTime);
-            BallRb.AddRelativeForce((u * Random.Range(-1.0f, 1.0f)) * speed * Time.deltaTime);
-        }
-        else
-            BallRb.AddForce(new Vector3(m.z, m.y, m.x));
     }
 
     public void OnCollisionEnter(Collision other)
     {
-        if(other.collider.tag == "Player")
-        {
-            moveTo = Vector3.right;
-            moveFrom = Vector3.forward;
-            //MaxAngularVelocity += 1.5f;
-           // speed += 1.0f;
-        }
-        else if(other.collider.tag == "Enemy")
-        {
-            moveTo = Vector3.left;
-            moveFrom = Vector3.back;
-           // MaxAngularVelocity += 1.5f;
-           // speed += 1.0f;
 
-        }
     }
 }

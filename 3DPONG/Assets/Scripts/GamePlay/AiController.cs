40c326bb4ae31a704f2362df84113a70a900adab
Josh Eugenio <Josh Eugenio>
2016-06-23 20:46:03 -0700

    Finished standard AI controller.

---
+++ 3DPONG/Assets/Scripts/GamePlay/AiController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AiController : MonoBehaviour {
+
+    public float speed = 50f;
+    public float negXBound = -4.4f;
+    public float posXBound = 4.4f;
+    public float negZBound = -4.16f;
+    public float posZBound = 2.66f;
+    float translationX;
+    float translationY;
+    Transform ball;
+    // Use this for initialization
+    void Start () {
+        ball = GameObject.Find("RollerBall").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ballX = ball.position.x;
+        float ballz = ball.position.z;
+
+        translationX = ballX * speed;
+        translationY = ballz * speed;
+        translationX *= Time.deltaTime;
+        translationY *= Time.deltaTime;
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x + translationX, negXBound, posXBound);
+        pos.z = Mathf.Clamp(pos.z + translationY, negZBound, posZBound);
+        transform.position = pos;
+         
+           
+	}
+}

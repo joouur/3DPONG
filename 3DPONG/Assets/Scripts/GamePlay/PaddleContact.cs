295829126b6134af848b6bd5bc3b807421ef41d7
Jorge Moreno <jmoreno@cgoswell.edu>
2016-07-14 19:42:38 -0700

    3d

--- 3DPONG/Assets/Scripts/GamePlay/PaddleContact.cs
+++ 3DPONG/Assets/Scripts/GamePlay/PaddleContact.cs
@@ -11,19 +11,22 @@ public class PaddleContact : MonoBehaviour
         if (!ball)
             ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Ball>();
     }
-    /*
+    
     public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "RollerBall")
         {
-            if (POrAI)
-                ball.BallRb.velocity = compVelocity(-other.contacts[0].normal, ball.speed);
-            else
-                ball.BallRb.velocity = -compVelocity(-other.contacts[0].normal, ball.speed);
+            ball.BallRb.velocity = compVelocity(-other.contacts[0].normal, ball.speed);
 
+            /*
+                if (POrAI)
+                    ball.BallRb.velocity = compVelocity(-other.contacts[0].normal, ball.speed);
+                else
+                    ball.BallRb.velocity = -compVelocity(-other.contacts[0].normal, ball.speed);
+    */
         }
     }
-    */
+    
     public Vector3 compVelocity(Vector3 normal, Vector3 velo)
     {
         Vector3 cTangnt = new Vector3((normal.x * Mathf.Cos(1.5707f) - normal.y * (float)Mathf.Sin(1.5707f)),

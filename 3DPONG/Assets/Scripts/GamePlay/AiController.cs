19d0118eced7e45d123072b1c3033a7e43926bd9
Josh Eugenio <Josh Eugenio>
2016-07-21 21:54:29 -0700

    FUCK YOUR BRANCHING RULES JORGE!!!!!!

--- 3DPONG/Assets/Scripts/GamePlay/AiController.cs
+++ 3DPONG/Assets/Scripts/GamePlay/AiController.cs
@@ -1,37 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AiController : MonoBehaviour {
+namespace Pong.Manager
+{
+    public class AiController : MonoBehaviour
+    {
 
-    public float speed = 25f;
-    public float negXBound = -3.96f;
-    public float posXBound = 3.993f;
-    public float negZBound = -3.21f;
-    public float posZBound = 3.21f;
-    float translationX;
-    float translationZ;
-    Transform ball;
-    // Use this for initialization
-    void Start () {  
-        ball = GameObject.FindGameObjectWithTag("Ball").transform;
-        GetComponent<Rigidbody>().isKinematic = true;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-       
-        if (ball.position.y < 0)
+        public float speed = 25f;
+        public float negXBound = -3.96f;
+        public float posXBound = 3.993f;
+        public float negZBound = -3.21f;
+        public float posZBound = 3.21f;
+        float translationX;
+        float translationZ;
+        Transform ball;
+        // Use this for initialization
+        void Start()
         {
-            float ballX = ball.position.x;
-            float ballz = ball.position.z;
-            Vector3 pos = transform.position;
-            pos.x = Mathf.Clamp(ballX, negXBound, posXBound);
-            pos.z = Mathf.Clamp(ballz, negZBound, posZBound);
-            transform.position = transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime*speed);
+            
+            GetComponent<Rigidbody>().isKinematic = true;
         }
-        
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            ball = GameObject.FindGameObjectWithTag("Ball").transform;
+            if (ball != null)
+            {
+                if (ball.position.y < 0)
+                {
+                    float ballX = ball.position.x;
+                    float ballz = ball.position.z;
+                    Vector3 pos = transform.position;
+                    pos.x = Mathf.Clamp(ballX, negXBound, posXBound);
+                    pos.z = Mathf.Clamp(ballz, negZBound, posZBound);
+                    transform.position = transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+                }
+            }
+            else
+            {
+                Vector3 pos = transform.position;
+                pos.x = Mathf.Clamp(0.43f, negXBound, posXBound);
+                pos.z = Mathf.Clamp(0.32f, negZBound, posZBound);
+                transform.position = transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+            }
+            
 
 
 
+
+        }
     }
 }

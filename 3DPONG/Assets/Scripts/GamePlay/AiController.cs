2d0bafdd2949a9876de86a7aa27f495b22712f33
Josh Eugenio <jeugenio93@gmail.com>
2016-08-09 19:57:19 -0700

    Finished Edge modifier for AI paddle

--- 3DPONG/Assets/Scripts/GamePlay/AiController.cs
+++ 3DPONG/Assets/Scripts/GamePlay/AiController.cs
@@ -13,6 +13,8 @@ namespace Pong.Manager
         public float posZBound = 3.21f;
         float translationX;
         float translationZ;
+        float hitModifier = 0f;
+        bool checkRand = false;
         Transform ball;
         Vector3 startPos = new Vector3(-3.094f, -24.15f, 2.801f);
         // Use this for initialization
@@ -29,12 +31,42 @@ namespace Pong.Manager
         {
             ball = GameObject.FindGameObjectWithTag("Ball").transform;
             if (ball != null)
-            {
+            {   //2.2f left and right edge
+                //1.3f up and down edge
+                float ballX;
+                float ballz;   
+                    
+               if (ball.position.y > 0 && !checkRand)
+                    checkRand = true;
                 if (ball.position.y < 0)
-                {
-                    float edgeModifier = Random.Range(0f, 70f);
-                    float ballX = ball.position.x;
-                    float ballz = ball.position.z;
+                {  
+                    if (checkRand)
+                    {
+                        hitModifier = Random.Range(30f, 70f);
+                        checkRand = false;
+                        Debug.Log("edgeModifier: " + hitModifier.ToString());
+                    }
+                    if (hitModifier >60 && hitModifier <= 70)
+                    {
+                        if (hitModifier > 60 && hitModifier <= 65)
+                            ballX = ball.position.x + 2.2f;
+                        else
+                            ballX = ball.position.x - 2.2f;
+                        ballz = ball.position.z;
+                    }
+                    else if (hitModifier > 50 && hitModifier <= 60 )
+                    {
+                        if (hitModifier > 50 && hitModifier <= 55)
+                            ballz = ball.position.z + 1.3f;
+                        else
+                            ballz = ball.position.z - 1.3f;
+                        ballX = ball.position.x;
+                    }
+                    else
+                    {
+                        ballX = ball.position.x;
+                        ballz = ball.position.z;
+                    }
                     Vector3 pos = transform.position;
                     pos.x = Mathf.Clamp(ballX, negXBound, posXBound);
                     pos.z = Mathf.Clamp(ballz, negZBound, posZBound);
@@ -48,11 +80,7 @@ namespace Pong.Manager
                 pos.z = Mathf.Clamp(0.32f, negZBound, posZBound);
                 transform.position = transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
             }
-            
-
-
-
-
+             
         }
     }
 }
